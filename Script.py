1349396df873b7507ee56d6de1f0504b8f8d835d
MiladEzame <milad.ezame@gmail.com>
2021-02-07 21:14:35 +0100

    Almost finished scraping a single book from url, data remaining to scrape : current url. Also need to correct the url and img data retrieved.

--- Script.py
+++ Script.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup as bs
 import csv
 import requests
 
-
 # information to gather :
 """
 product_page_url not ok
@@ -12,7 +11,7 @@ price_including_tax ok
 price_excluding_tax ok
 number_available ok
 product_description ok
-category not ok
+category ok
 review_rating ok
 image_url ok
 """
@@ -20,50 +19,61 @@ image_url ok
 # for multiple pages, change the number into a variable to increment 
 # example with this url : https://books.toscrape.com/catalogue/category/books/sequential-art_5/page-[number_to_increment].html
 
+csv_headers = []
+csv_values = []
+
 # url of the website to scrape
-URL = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+URL = "https://books.toscrape.com/catalogue/sharp-objects_997/index.html"
 
 # attempt to access the url, if req = 200 -> (success) 
 # instantiate a soup object with 2 parameters : the data content(can be text or other methods) needed and the type to parse  
 req = requests.get(URL)
 soup = bs(req.text, "html.parser")
-req = requests.get(URL)
-soup = bs(req.text, "html.parser")
 soup.prettify("utf-8")
-csv_row1 = []
-csv_row2 = []
-# find the tags
-book_title = soup.find("h1").text.strip()
-image_url = soup.img["src"]
-category = soup.find("ul",attrs={"class":"breadcrumb"}).find_all("a")[2:]
+# still cannot write properly £ into the csv file
 
-print(category)
+# cannot get anything different than ../../index.html, looking for get_current_url() function ?
+def add_current_url():
+    current_url = soup.find("div",{"class":"col-sm-8 h1"}).find("a").get("href")
+    #url_list.append(current_url)
+    #header_list.append("Current_Url")
+    print(current_url)
 
+def add_data_category(category_list,header_list):
+    for categories in soup.findAll("ul",{"class":"breadcrumb"}):
+        for category in soup.findAll("a")[3:]:
+            category_list.append(category.get_text())
+    header_list.append("Category")
 
-csv_row1.append("Title")
-csv_row1.append("Image_url")
-csv_row1.append("Category")
+def add_data_img(image_list,header_list):
+    image_url = soup.img["src"]
+    image_list.append(image_url)
+    header_list.append("Image_url")
 
-csv_row2.append(book_title)
-csv_row2.append(image_url)
-csv_row2.append(category)
+def add_data_title(title_list,header_list):
+    book_title = soup.find("h1").text.strip()
+    title_list.append(book_title)
+    header_list.append("Title")
 
+def write_file(header_list, value_list):
+    # write everything in a csv file
+    add_data_category(csv_values,csv_headers)
+    add_data_img(csv_values,csv_headers)
+    add_data_title(csv_values,csv_headers)
+    with open('book_scraped.csv', 'w',newline="") as file:
+        writer = csv.writer(file)
 
+        # loop into the list and get all the labels
+        for header in soup.findAll("table",{"class":"table table-striped"}):
+            for header_text in soup.findAll("th"):
+                header_list.append(header_text.get_text())
+            writer.writerow(header_list)
 
-# write everything in a csv file
-with open('book_scraped.csv', 'w',newline="") as file:
-    writer = csv.writer(file)
+        # loop into the list and get all the values
+        for value in soup.findAll("table",{"class":"table table-striped"}):
+            for value_text in soup.findAll("td"):
+                value_list.append(value_text.get_text())
+            writer.writerow(value_list)
+    file.close()
 
-    # loop into the list and get all the labels
-    for header in soup.findAll("table"):
-        for header_text in soup.findAll("th"):
-            csv_row1.append(header_text.get_text())
-        writer.writerow(csv_row1)
-
-    # loop into the list and get all the values
-    for value in soup.findAll("table"):
-        for value_text in soup.findAll("td"):
-            csv_row2.append(value_text.get_text())
-        writer.writerow(csv_row2)
-
-file.close()
+write_file(csv_headers,csv_values)
